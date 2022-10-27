70a2817e143265bf0dd3183439ebd00af4676cf0
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2022-10-27 16:28:09 +0200

    Add unfinished solution of exercise "Log Level"

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,9 +1,30 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
     # Please implement the to_label/2 function
-  end
+    cond do
+      level == 0 and legacy? == false -> :trace
+      level == 1 and legacy? == true -> :debug
+      level == 2 and legacy? == true -> :info
+      level == 3 and legacy? == true -> :warning
+      level == 4 and legacy? == true -> :error
+      level == 5 and legacy? == false -> :fatal
+      true  -> :unknown
+    end
+end
 
   def alert_recipient(level, legacy?) do
     # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+    cond do
+      label == :unknown and legacy? == true -> :dev1
+      label == :unknown and legacy? == false -> :dev2
+      label == :error or :fatal -> :ops
+
+      label == :warning -> :ignore
+      label == :info -> :ignore
+      label == :debug -> :ignore
+      label == :trace -> :ignore
+      true -> :ignore
+    end
   end
 end
